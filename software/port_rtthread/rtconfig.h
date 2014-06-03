81ec5fb79f9714d54fbb37a2ea80f982c8339f16
xiaonong <nongxiaoming@gmail.com>
2014-06-03 21:19:01 +0800

    给rtconfig配置添加中午配置。

--- software/port_rtthread/rtconfig.h
+++ software/port_rtthread/rtconfig.h
@@ -2,120 +2,88 @@
 #ifndef __RTTHREAD_CFG_H__
 #define __RTTHREAD_CFG_H__
 
-// <RDTConfigurator URL="http://www.rt-thread.com/eclipse">
-
-// <integer name="RT_NAME_MAX" description="Maximal size of kernel object name length" default="6" />
+/* 最大对象名称长度 */
 #define RT_NAME_MAX	8
-// <integer name="RT_ALIGN_SIZE" description="Alignment size for CPU architecture data access" default="4" />
+/* 默认对齐方式 */
 #define RT_ALIGN_SIZE	4
-// <integer name="RT_THREAD_PRIORITY_MAX" description="Maximal level of thread priority" default="32">
-// <item description="8">8</item>
-// <item description="32">32</item>
-// <item description="256">256</item>
-// </integer>
+/* 最大支持优先级数 */
 #define RT_THREAD_PRIORITY_MAX	32
-// <integer name="RT_TICK_PER_SECOND" description="OS tick per second" default="100" />
+/* 每秒的tick个数 */
 #define RT_TICK_PER_SECOND	1000
-// <integer name="IDLE_THREAD_STACK_SIZE" description="The stack size of idle thread" default="512" />
+/* 空闲任务的栈大小 */
 #define IDLE_THREAD_STACK_SIZE	512
-// <bool name="RT_USING_MODULE" description="Using Application Module" default="true" />
-//#define RT_USING_MODULE
-// <section name="RT_DEBUG" description="Kernel Debug Configuration" default="true" >
+/* 是否使用内核调试 */
 #define RT_DEBUG
-// <bool name="RT_THREAD_DEBUG" description="Thread debug enable" default="false" />
-// #define RT_THREAD_DEBUG
-// <bool name="RT_USING_OVERFLOW_CHECK" description="Thread stack over flow detect" default="true" />
+/* 是否使用栈溢出检查 */
 #define RT_USING_OVERFLOW_CHECK
-// </section>
-
-// <bool name="RT_USING_HOOK" description="Using hook functions" default="true" />
+/* 是否使用钩子函数 */
 #define RT_USING_HOOK
-// <section name="RT_USING_TIMER_SOFT" description="Using software timer which will start a thread to handle soft-timer" default="true" >
+/* 是否使用软件timer */
 #define RT_USING_TIMER_SOFT
-// <integer name="RT_TIMER_THREAD_PRIO" description="The priority level of timer thread" default="4" />
+/* timer线程优先级 */
 #define RT_TIMER_THREAD_PRIO	4
-// <integer name="RT_TIMER_THREAD_STACK_SIZE" description="The stack size of timer thread" default="512" />
+/* timer线程栈大小 */
 #define RT_TIMER_THREAD_STACK_SIZE	512
-// <integer name="RT_TIMER_TICK_PER_SECOND" description="The soft-timer tick per second" default="10" />
+/* timer每秒的tick个数 */
 #define RT_TIMER_TICK_PER_SECOND	100
-// </section>
 
-// <section name="IPC" description="Inter-Thread communication" default="always" >
-// <bool name="RT_USING_SEMAPHORE" description="Using semaphore in the system" default="true" />
+/* 是否使用信号量 */
 #define RT_USING_SEMAPHORE
-// <bool name="RT_USING_MUTEX" description="Using mutex in the system" default="true" />
+/* 是否使用互斥量 */
 #define RT_USING_MUTEX
-// <bool name="RT_USING_EVENT" description="Using event group in the system" default="true" />
+/* 是否使用事件 */
 #define RT_USING_EVENT
-// <bool name="RT_USING_MAILBOX" description="Using mailbox in the system" default="true" />
+/* 是否使用邮箱 */
 #define RT_USING_MAILBOX
-// <bool name="RT_USING_MESSAGEQUEUE" description="Using message queue in the system" default="true" />
+/* 是否使用消息队列 */
 #define RT_USING_MESSAGEQUEUE
-// </section>
 
-// <section name="MM" description="Memory Management" default="always" >
-// <bool name="RT_USING_MEMPOOL" description="Using Memory Pool Management in the system" default="true" />
+/* 是否使用内存池 */
 #define RT_USING_MEMPOOL
-// <bool name="RT_USING_HEAP" description="Using Dynamic Heap Management in the system" default="true" />
+/* 是否使用内存管理 */
 #define RT_USING_HEAP
-// <bool name="RT_USING_MEMHEAP" description="Using extra-dynamic heap management in the system" default="true" />
+/* 是否使用动态内存管理 */
 #define RT_USING_MEMHEAP
-// <bool name="RT_USING_SMALL_MEM" description="Optimizing for small memory" default="false" />
+/* 是否使用优化的小型内存管理 */
 #define RT_USING_SMALL_MEM
-// <bool name="RT_USING_SLAB" description="Using SLAB memory management for large memory" default="false" />
-// #define RT_USING_SLAB
-// </section>
 
-// <section name="RT_USING_DEVICE" description="Using Device Driver Framework" default="true" >
+/* 是否使用设备框架 */
 #define RT_USING_DEVICE
-// <bool name=RT_USING_DEVICE_IPC description="Using IPC in Device Driver Framework" default="true" />
+/* 是否使用IPC设备框架 */
 #define RT_USING_DEVICE_IPC
-// <bool name="RT_USING_SERIAL" description="Using Serial Device Driver Framework" default="true" />
+/* 是否使用串口设备框架 */
 #define RT_USING_SERIAL
-// <integer name="RT_UART_RX_BUFFER_SIZE" description="The buffer size for UART reception" default="64" />
+/* 串口设备接收缓冲区大小 */
 #define RT_UART_RX_BUFFER_SIZE    64
-// <bool name="RT_USING_SPI" description="Using SPI Device Driver Framework" default="true" />
+/* 是否使用SPI设备框架 */
 #define RT_USING_SPI
-// <bool name="RT_USING_I2C" description="Using I2C Device Driver Framework" default="true" />
+/* 是否使用I2C设备框架 */
 #define RT_USING_I2C
-// <bool name="RT_USING_RTC" description="Using RTC Device Driver Framework" default="true" />
+/* 是否使用RTC框架 */
 //#define RT_USING_RTC
-// <integer name="RT_MMCSD_THREAD_PREORITY" description="The prority of mmcsd thread" default="15" />
-#define RT_MMCSD_THREAD_PREORITY	15
-// <section name="RT_USING_CONSOLE" description="Using console" default="true" >
+/* 是否使用输出模块 */
 #define RT_USING_CONSOLE
-// <integer name="RT_CONSOLEBUF_SIZE" description="The buffer size for console output" default="128" />
+/* 输出缓冲区大小 */
 #define RT_CONSOLEBUF_SIZE	128
-// <string name="RT_CONSOLE_DEVICE_NAME" description="The device name for console" default="uart" />
+/* 用于输出的设备名称 */
 #define RT_CONSOLE_DEVICE_NAME	"uart2"
-// </section>
 
-// <bool name="RT_USING_COMPONENTS_INIT" description="Using RT-Thread components initialization" default="true" />
-//#define RT_USING_COMPONENTS_INIT
-// <section name="RT_USING_FINSH" description="Using finsh as shell, which is a C-Express shell" default="true" >
+/* 是否使用finsh调试组件 */
 #define RT_USING_FINSH
-// <bool name="FINSH_USING_SYMTAB" description="Using symbol table in finsh shell" default="true" />
+/* 是否使用finsh命令表 */
 #define FINSH_USING_SYMTAB
-// <bool name="FINSH_USING_DESCRIPTION" description="Keeping description in symbol table" default="true" />
 #define FINSH_USING_DESCRIPTION
-// <integer name="FINSH_THREAD_STACK_SIZE" description="The stack size for finsh thread" default="4096" />
+/* finsh任务栈大小 */
 #define FINSH_THREAD_STACK_SIZE	4096
-// <bool name="FINSH_USING_MSH" description="Using module shell in finsh" default="true" />
+/* 是否使用MSH模式 */
 //#define FINSH_USING_MSH
-// </section>
 
-// <section name="LIBC" description="C Runtime library setting" default="always" >
-// <bool name="RT_USING_NEWLIB" description="Using newlib library, only available under GNU GCC" default="true" />
-// #define RT_USING_NEWLIB
-// <bool name="RT_USING_PTHREADS" description="Using POSIX threads library" default="true" />
-//#define RT_USING_PTHREADS
-// <bool name="RT_USING_ARM_LIBC" description="Using arm c library" default="true" />
+/* 是否使用arm 标准C库 */
 #define RT_USING_ARM_LIBC
-// </section>
 
-// <section name="RT_USING_DFS" description="Device file system" default="true" >
+/* 是否使用DFS文件系统 */
 //#define RT_USING_DFS
-// <bool name="DFS_USING_WORKDIR" description="Using working directory" default="true" />
+/* 是否使用工作目录模式 */
 #define DFS_USING_WORKDIR
 // <integer name="DFS_FILESYSTEM_TYPES_MAX" description="The maximal number of the supported file system type" default="4" />
 #define DFS_FILESYSTEM_TYPES_MAX  4
@@ -151,11 +119,10 @@
 // #define RT_USING_DFS_UFFS
 // <bool name="RT_USING_DFS_DEVFS" description="Using devfs for device objects" default="true" />
 #define RT_USING_DFS_DEVFS
-// <bool name="RT_USING_DFS_ROMFS" description="Using ROMFS" default="false" />
-//#define RT_USING_DFS_ROMFS
-// <bool name="RT_USING_DFS_NFS" description="Using NFS" default="false" />
+
+/* 是否使用NFS */
 #define RT_USING_DFS_NFS
-// <string name="RT_NFS_HOST_EXPORT" description="The exported NFS host path" default="192.168.1.10:/" />
+/* NFS端口信息 */
 #define RT_NFS_HOST_EXPORT  "192.168.1.20:/"
 // </section>
 
